afaa0d882e2f54afdd2ee75454b35a1dbc9f21bd
Matt Varughese <matt@monolithai.com>
2020-06-18 15:38:36 +0100

    new code

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const {Octokit} = require("@octokit/action");
 
-try {
-  // `changelog_file input defined in action metadata file
-  const changelogLocation = core.getInput('changelog_file');
-  console.log(`changlelog file location ${changelogLocation}`);
-  // Get the JSON webhook payload for the event that triggered the workflow
+async function main() {
+  try {
+    // `changelog_file input defined in action metadata file
+    const changelogLocation = core.getInput('changelog_file');
+    console.log(`changlelog file location ${changelogLocation}`);
+    // Get the JSON webhook payload for the event that triggered the workflow
 
-  const payload = github.context.payload
-  const {number} = payload;
-  const pull_number = number;
+    const payload = github.context.payload
+    const {number} = payload;
+    const pull_number = number;
 
-  if (!pull_number) {
-    console.log('This action was initiated by something other than a pull_request');
-    process.exit(0);
+    if (!pull_number) {
+      console.log('This action was initiated by something other than a pull_request');
+      process.exit(0);
+    }
+
+    console.log(`Getting commits for pull_number: ${pull_number}`);
+    console.log(`Searching for: ${changelogLocation}`);
+
+    const files = await octokit.pulls.listFiles({owner, repo, pull_number});
+    const payload_string = JSON.stringify(github.context.payload, undefined, 2)
+    const files_string = JSON.stringify(files, undefined, 2)
+    console.log(`The event payload: ${payload_string}`);
+    console.log(`The PR files information: ${files_string}`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  console.log(`Getting commits for pull_number: ${pull_number}`);
-  console.log(`Searching for: ${changelogLocation}`);
-
-  const files = await octokit.pulls.listFiles({owner, repo, pull_number});
-  const payload_string = JSON.stringify(github.context.payload, undefined, 2)
-  const files_string = JSON.stringify(files, undefined, 2)
-  console.log(`The event payload: ${payload_string}`);
-  console.log(`The PR files information: ${files_string}`);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+main();
