0e0a277aa5ca16433cbfee3517c055527373eae9
Matt Varughese <matt@monolithai.com>
2020-06-18 15:51:44 +0100

    added owner

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ async function main() {
     // `changelog_file input defined in action metadata file
     const changelogLocation = core.getInput('changelog_file');
     const octokit = new Octokit();
-    
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
     console.log(`changlelog file location ${changelogLocation}`);
     // Get the JSON webhook payload for the event that triggered the workflow
 
