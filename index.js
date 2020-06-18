9751f89ee6d472fb7dfc5ae53237ab18f21db685
Matt Varughese <matt@monolithai.com>
2020-06-18 15:34:42 +0100

    Added octokit and updated index to log PR files

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {Octokit} = require("@octokit/action");
 
 try {
   // `changelog_file input defined in action metadata file
   const changelogLocation = core.getInput('changelog_file');
-  console.log(`changlelog file location ${changelogLocation}!`);
+  console.log(`changlelog file location ${changelogLocation}`);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+  const payload = github.context.payload
+  const {number} = payload;
+  const pull_number = number;
+
+  if (!pull_number) {
+    console.log('This action was initiated by something other than a pull_request');
+    process.exit(0);
+  }
+
+  console.log(`Getting commits for pull_number: ${pull_number}`);
+  console.log(`Searching for: ${changelogLocation}`);
+
+  const files = await octokit.pulls.listFiles({owner, repo, pull_number});
+  const payload_string = JSON.stringify(github.context.payload, undefined, 2)
+  const files_string = JSON.stringify(files, undefined, 2)
+  console.log(`The event payload: ${payload_string}`);
+  console.log(`The PR files information: ${files_string}`);
 } catch (error) {
   core.setFailed(error.message);
 }
